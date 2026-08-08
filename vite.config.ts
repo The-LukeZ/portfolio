57cfc01e81e5bf29db97704ff4fc8cd84beb814f
The-LukeZ <contact.lukez@proton.me>
2026-08-08 16:52:30 +0200

    fix: update types path in tsconfig.json and correct import statement in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { paraglideVitePlugin } from "@inlang/paraglide-js";
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 import devtoolsJson from "vite-plugin-devtools-json";
-import { generateProjectsPlugin } from "./vite-plugins/generate-projects";
+import { generateProjectsPlugin } from "./vite-plugins/generate-projects.js";
 
 export default defineConfig({
   plugins: [
